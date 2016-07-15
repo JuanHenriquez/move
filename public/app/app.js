b7aee33596d38f24622a1899bab14464646c0460
Juan Henriquez <juanhenriquez@Juans-MacBook-Air.local>
2016-07-15 00:33:51 -0700

    Configure the theme app and add the main controller.

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,17 @@
-angular.module('moveApp', ['ngMaterial'])
+var app = angular.module('moveApp', ['ngMaterial']);
 
-.controller('NavController', function($scope) {
-    $scope.title = "Move";
-})
+app.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue-grey', {
+            'default': '900',
+            'hue-1': '800',
+            'hue-2': '600'
+        })
+        .accentPalette('pink')
+        .warnPalette('deep-orange');
+});
+
+app.controller('AppController', ['$scope', function ($scope) {
+    $scope.username = "Juan Henriquez";
+    $scope.userEmail = "juanhenriquezzavarse18@gmail.com"
+}]);
