4a0920d7c5122cea6454a450c9ed723eb3e8d40e
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-07-19 23:28:25 -0700

    Main file app for angular logic.
    - Configure the Angular Material Color Schemas.
    - Set the first routes

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,50 @@
-var app = angular.module('moveApp', ['ngMaterial']);
+var angular = require('angular');
 
-app.config(function($mdThemingProvider) {
+// requires
+require('angular-animate');
+require('angular-aria');
+require('angular-material');
+require('angular-ui-router');
+
+var app = angular.module('moveApp', ['ngMaterial', 'ui.router']);
+
+appConfig.$inject = ['$mdThemingProvider', '$stateProvider', '$urlRouterProvider'];
+
+app.config(appConfig);
+
+function appConfig ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
+
+    // Config the app theme.
     $mdThemingProvider.theme('default')
         .primaryPalette('blue-grey', {
             'default': '900',
             'hue-1': '800',
-            'hue-2': '600'
+            'hue-2': '600',
+            'hue-3': '50'
         })
         .accentPalette('pink')
         .warnPalette('deep-orange');
-});
 
-app.controller('AppController', ['$scope', function ($scope) {
-    $scope.username = "Juan Henriquez";
-    $scope.userEmail = "juanhenriquezzavarse18@gmail.com"
-}]);
+
+    // Config the app SPA routes.
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: './app/views/home.html',
+            controller: 'PostsController'
+        })
+
+        .state('friends', {
+            url: '/friends',
+            templateUrl: './app/views/friends.html'
+        })
+}
+
+// Services.
+require('./services');
+// Controllers.
+require('./controllers');
+// Components or Directives.
+require('./components');
