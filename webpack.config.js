a98398a52746d74be06995c7bd686e8adc1325e4
Juan Henriquez <juanhenriquez@Juans-MacBook-Air.local>
2016-07-19 23:02:21 -0700

    Add webpack for bundle all the angular app in one file.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './public/app/app.js',
+        vendor: ['angular', 'angular-animate', 'angular-aria', 'angular-material', 'angular-ui-router']
+    },
+    output: {
+        path: './public/js',
+        filename: 'app.bundle.js'
+    },
+    plugins: [new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")]
+};
